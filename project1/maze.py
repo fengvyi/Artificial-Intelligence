b7251a0bb9ed28c0bd94c32e62ba4bbc3cef2e16
fengvyi <zefengsong@gwmail.gwu.edu>
2018-02-01 17:27:48 -0500

    Create maze.py

---
+++ project1/maze.py
@@ -0,0 +1,39 @@
+
+file1 = open("maze.txt", "r")
+read = file1.read()
+
+# Find a path from (a, b) to (x, y) using Depth-first search
+def DFS(a, b, x, y, maze):
+    # Target found, return
+    if a == x and b == y:
+        return True
+    # Hit a wall or re-visited previous cell, return
+    if maze[a][b] or a < 0 or a > 80 or b < 0 or b > 80:
+        return False
+    # Set visited cell to 2
+    maze[a][b] = 2
+    # Call recursively
+    return DFS(a + 1, b, x, y, maze) or DFS(a, b + 1, x, y, maze) or DFS(a - 1, b, x, y, maze) or DFS(a, b - 1, x, y, maze)
+
+
+
+row, col = 0, 0
+
+maze = [[0 for x in range(81)] for y in range(81)]
+
+# Initialize the matrix
+for x in read:
+    if x == '1' or x == '0':
+        maze[row][col] = int(x)
+        col += 1
+        if col == 81:
+            row += 1
+            col = 0
+
+a, b = 1, 75
+x, y = 37, 77
+
+result = DFS(a, b, x, y, maze)
+
+print "Path from (%s, %s) to (%s, %s): %s" %(a, b, x, y, result)
+
