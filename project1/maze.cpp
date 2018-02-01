55f63bc2e90e8dd3bf17f8952bc580f9943255e1
fengvyi <zefengsong@gwmail.gwu.edu>
2018-02-01 16:56:43 -0500

    Update maze.cpp

--- project1/maze.cpp
+++ project1/maze.cpp
@@ -4,16 +4,13 @@
 #include <vector>
 using namespace std;
 
+// Find a path from (a, b) to (x, y) using Depth-first search
 bool DFS(int a, int b, int x, int y, vector<vector<int>>& maze) {
-	cout << a << "," << b << endl;
-	if (a == x && b == y) {
-		return true;
-	}
+	//cout << a << "," << b << endl;
+	if (a == x && b == y) return true;
 
-	if (maze[a][b] || a < 0 || a > 80 || b < 0 || b > 80) {
-		return false;
-	}
-
+	if (maze[a][b] || a < 0 || a > 80 || b < 0 || b > 80) return false;
+	
 	maze[a][b] = 2;
 
 	return DFS(a + 1, b, x, y, maze) || DFS(a, b + 1, x, y, maze) || DFS(a - 1, b, x, y, maze) || DFS(a, b - 1, x, y, maze);
@@ -46,13 +43,16 @@ int main() {
 	int a = 1, b = 75, x = 37, y = 77;
 
 	bool result = DFS(a, b, x, y, maze);
-	cout << result << endl;
-	//for (auto i : maze) {
-	//	for (auto j : i) {
-	//		cout << j << " ";
-	//	}
-	//	cout << endl;
-	//}
+	
+	cout << "Result of finding path from ("<<a<<","<<b<<") to ("<<x<<","<<y<<") is:" << endl;
+	cout << (result ? "True" : "False") << endl;
+
+	// for (auto i : maze) {
+	//	 for (auto j : i) {
+	//	 	 cout << j << " ";
+	//	 }
+	//	 cout << endl;
+	// }
 
 	inFile.close();
 
